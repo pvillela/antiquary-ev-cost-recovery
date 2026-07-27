9bd1210befc2b77b3c53e6290eadd95d71d60739
pvillela <pvillela@gmail.com>
2026-07-26 23:09:58 -0400

    Replaced lifetimed references with with Rc in core src/grouping.rs types. Completed src/peak_est.rs.

--- src/grouping.rs
+++ src/grouping.rs
@@ -1,6 +1,10 @@
 use crate::quicksort;
 use jiff::{Timestamp, Zoned, tz::TimeZone};
-use std::{collections::BTreeSet, time::Duration};
+use std::{
+    collections::{BTreeSet, btree_set::Iter},
+    rc::Rc,
+    time::Duration,
+};
 
 /// Sessions whose overlap with the interval of interest is less than or equal to this
 /// are excluded from the calculations.
@@ -69,24 +73,24 @@ impl Ord for Session {
 }
 
 #[derive(Debug, PartialEq, PartialOrd)]
-enum EndPoint<'a> {
-    Left(EndPointData<'a>),
-    Right(EndPointData<'a>),
+enum EndPoint {
+    Left(EndPointData),
+    Right(EndPointData),
 }
 
 #[derive(Debug, Clone)]
-struct EndPointData<'a> {
+struct EndPointData {
     time: Timestamp,
-    session: &'a Session,
+    session: Rc<Session>,
 }
 
-impl<'a> PartialEq for EndPointData<'a> {
+impl PartialEq for EndPointData {
     fn eq(&self, other: &Self) -> bool {
         self.time == other.time && self.session.id == other.session.id
     }
 }
 
-impl<'a> PartialOrd for EndPointData<'a> {
+impl PartialOrd for EndPointData {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         match self.time.partial_cmp(&other.time) {
             Some(core::cmp::Ordering::Equal) => {}
@@ -99,13 +103,26 @@ impl<'a> PartialOrd for EndPointData<'a> {
 #[derive(Debug, Clone)]
 /// Collection of sessions that have non-trivial intersections with each other and with the interval
 /// of interest.
-pub struct SessionGroup<'a> {
+pub struct SessionGroup {
     start: Timestamp,
     end: Timestamp,
-    pub(crate) sessions: BTreeSet<&'a Session>,
+    sessions: BTreeSet<Rc<Session>>,
 }
 
-impl<'a> Default for SessionGroup<'a> {
+pub struct SessionIter<'a>(Iter<'a, Rc<Session>>);
+
+impl<'a> Iterator for SessionIter<'a> {
+    type Item = &'a Session;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.0.next() {
+            None => None,
+            Some(rc) => Some(rc.as_ref()),
+        }
+    }
+}
+
+impl Default for SessionGroup {
     fn default() -> Self {
         Self {
             start: Timestamp::MIN,
@@ -115,15 +132,27 @@ impl<'a> Default for SessionGroup<'a> {
     }
 }
 
-impl<'a> SessionGroup<'a> {
-    fn new(data: EndPointData<'a>) -> Self {
-        let mut sessions = BTreeSet::new();
+impl SessionGroup {
+    fn new(data: EndPointData) -> Self {
+        let mut sessions = BTreeSet::<Rc<Session>>::new();
         sessions.insert(data.session);
         Self {
             start: data.time,
             end: Timestamp::MAX,
             sessions,
         }
+    }
+
+    pub fn start(&self) -> Timestamp {
+        self.start
+    }
+
+    pub fn end(&self) -> Timestamp {
+        self.end
+    }
+
+    pub fn session_iter<'a>(&'a self) -> SessionIter<'a> {
+        SessionIter(self.sessions.iter())
     }
 
     pub fn agg_avg_power(&self) -> f64 {
@@ -138,10 +167,10 @@ impl<'a> SessionGroup<'a> {
         Duration::try_from(self.end - self.start).expect("span should be at most a few hours")
     }
 
-    fn remove_sessions(&mut self, remove_list: &mut Vec<&Session>) -> Self {
+    fn remove_sessions(&mut self, remove_list: &mut Vec<Rc<Session>>) -> Self {
         let old_group = self.clone();
         remove_list.iter().for_each(|s| {
-            self.sessions.remove(*s);
+            self.sessions.remove(s);
         });
         self.end = Timestamp::MAX;
         old_group
@@ -149,14 +178,14 @@ impl<'a> SessionGroup<'a> {
 }
 
 #[derive(Default)]
-struct GroupState<'a> {
-    groups: Vec<SessionGroup<'a>>,
-    curr_group: SessionGroup<'a>,
-    remove_list: Vec<&'a Session>,
+struct GroupState {
+    groups: Vec<SessionGroup>,
+    curr_group: SessionGroup,
+    remove_list: Vec<Rc<Session>>,
 }
 
-impl<'a> GroupState<'a> {
-    fn process_left_edge(&mut self, data: EndPointData<'a>) {
+impl GroupState {
+    fn process_left_edge(&mut self, data: EndPointData) {
         let group = &mut self.curr_group;
         if group.sessions.is_empty() {
             self.curr_group = SessionGroup::new(data);
@@ -176,7 +205,7 @@ impl<'a> GroupState<'a> {
         }
     }
 
-    fn process_right_edge(&mut self, data: EndPointData<'a>) {
+    fn process_right_edge(&mut self, data: EndPointData) {
         let group = &mut self.curr_group;
         if group.sessions.is_empty() {
             panic!("illegal state");
@@ -205,33 +234,33 @@ impl<'a> GroupState<'a> {
 //
 //       |2-|3-----|2--|3-----|2--|1--|
 
-pub fn groups_for_interval<'a>(
+pub fn groups_for_interval(
     interval: (Timestamp, Timestamp),
-    sessions: &'a [Session],
-) -> Vec<SessionGroup<'a>> {
+    sessions: &[Rc<Session>],
+) -> Vec<SessionGroup> {
     let end_points = end_points_for_interval(interval, sessions, OVERLAP_THRESHOLD);
     end_points_to_groups(end_points)
 }
 
 /// Returns an unsorted list of end-points corresponding to the sessions that non-trivially intersect
 /// `interval`.
-fn end_points_for_interval<'a>(
+fn end_points_for_interval(
     interval: (Timestamp, Timestamp),
-    sessions: &'a [Session],
+    sessions: &[Rc<Session>],
     overlap_threshold: Duration,
-) -> Vec<EndPoint<'a>> {
+) -> Vec<EndPoint> {
     let lo = interval.0 + overlap_threshold;
     let hi = interval.1 - overlap_threshold;
-    let mut end_points = Vec::<EndPoint<'a>>::new();
+    let mut end_points = Vec::<EndPoint>::new();
     for session in sessions {
         if session.conn_start < hi && session.conn_end >= lo {
             let left = EndPoint::Left(EndPointData {
                 time: session.conn_start.max(lo),
-                session,
+                session: session.clone(),
             });
             let right = EndPoint::Right(EndPointData {
                 time: session.conn_end.min(hi),
-                session,
+                session: session.clone(),
             });
             end_points.push(left);
             end_points.push(right);
@@ -240,7 +269,7 @@ fn end_points_for_interval<'a>(
     end_points
 }
 
-fn end_points_to_groups<'a>(mut end_points: Vec<EndPoint<'a>>) -> Vec<SessionGroup<'a>> {
+fn end_points_to_groups(mut end_points: Vec<EndPoint>) -> Vec<SessionGroup> {
     quicksort(&mut end_points);
 
     let mut state = GroupState::default();
@@ -256,11 +285,11 @@ fn end_points_to_groups<'a>(mut end_points: Vec<EndPoint<'a>>) -> Vec<SessionGro
 #[cfg(test)]
 // cargo test --package ev-peak-contrib --lib --all-features -- peak_contrib::test --nocapture
 mod test {
-    use crate::{Session, grouping::EndPoint, grouping::EndPointData};
+    use super::*;
 
     #[test]
     fn test_end_point_kind_order() {
-        let session = Session {
+        let session = Rc::new(Session {
             id: Default::default(),
             conn_start: Default::default(),
             raw_conn_end: Default::default(),
@@ -268,14 +297,14 @@ mod test {
             charge_time: Default::default(),
             energy_use: Default::default(),
             avg_power: Default::default(),
-        };
+        });
         let data1 = EndPointData {
             time: Default::default(),
-            session: &session,
+            session: session.clone(),
         };
         let data2 = EndPointData {
             time: Default::default(),
-            session: &session,
+            session: session.clone(),
         };
         assert!(EndPoint::Left(data1) < EndPoint::Right(data2));
     }
--- src/peak_est.rs
+++ src/peak_est.rs
@@ -1,59 +1,77 @@
-use crate::{SessionGroup, SessionReport, groups_for_interval, session_list};
+use crate::{Session, SessionGroup, SessionReport, groups_for_interval, session_list};
 use jiff::Timestamp;
-use std::{error::Error, path::Path};
+use std::{error::Error, path::Path, rc::Rc};
 
 pub const EV_POWER_FACTOR: f64 = 0.95;
 pub const EVOLUTE_BREAKER_KW_RATING: f64 = 6.7;
 pub const EVOLUTE_BREAKER_KVA_RATING: f64 = 7.5;
 
-pub struct PowerEstimate<'a> {
-    value: f64,
-    session_group: SessionGroup<'a>,
+pub struct PowerEstimate {
+    pub value: f64,
+    pub session_group: SessionGroup,
 }
 
-pub struct PowerEstimates<'a> {
-    pub consumption_based_kw: PowerEstimate<'a>,
-    pub consumption_based_kva: PowerEstimate<'a>,
-    pub breaker_specs_based_kw: PowerEstimate<'a>,
-    pub breaker_specs_based_kva: PowerEstimate<'a>,
+pub struct PowerEstimates {
+    pub consumption_based_kw: PowerEstimate,
+    pub consumption_based_kva: PowerEstimate,
+    pub breaker_specs_based_kw: PowerEstimate,
+    pub breaker_specs_based_kva: PowerEstimate,
 }
 
-pub struct PowerEstimatesReport<'a> {
-    pub estimates: PowerEstimates<'a>,
-    pub session_report: SessionReport,
+pub struct PowerEstimatesReport {
+    pub estimates: PowerEstimates,
+    pub spikes: Vec<Session>,
+    pub warnings: Vec<String>,
 }
 
-// pub fn max_power_estimates_for_interval<'a>(
-//     interval: (Timestamp, Timestamp),
-//     path: &Path,
-// ) -> Result<PowerEstimatesReport<'a>, Box<dyn Error>> {
-//     let session_report = session_list(path)?;
-//     let sessions = session_report.sessions;
-//     let groups = groups_for_interval(interval, &sessions);
+/// Produces EV maximum power estimates for the interval of interest `interval` and the session
+/// report at `path`.
+pub fn max_power_estimates_for_interval(
+    interval: (Timestamp, Timestamp),
+    path: &Path,
+) -> Result<PowerEstimatesReport, Box<dyn Error>> {
+    let session_report = session_list(path)?;
+    let SessionReport { sessions, spikes } = session_report;
 
-//     let (consumption_based_kw, consumption_based_kva) =
-//         max_kw_kva_over_groups_based_on_consumption(&groups);
-//     let (breaker_specs_based_kw, breaker_specs_based_kva) =
-//         max_kw_kva_over_groups_based_on_breaker_specs(&groups);
+    // Prepare warnings.
+    let mut warnings = Vec::<String>::new();
+    if !spikes.is_empty() {
+        warnings.push("spikes need to be reviewed".to_owned());
+    }
+    let intersects = sessions
+        .iter()
+        .any(|s| s.conn_start < interval.1 && s.raw_conn_end > interval.0);
+    if !intersects {
+        warnings.push("interval does not intersect any session in the report".to_owned());
+    }
 
-//     let estimates = PowerEstimates {
-//         consumption_based_kw,
-//         consumption_based_kva,
-//         breaker_specs_based_kw,
-//         breaker_specs_based_kva,
-//     };
+    let session_refs: Vec<_> = sessions.into_iter().map(|s| Rc::new(s)).collect();
+    let groups = groups_for_interval(interval, &session_refs);
 
-//     Ok(PowerEstimatesReport {
-//         estimates,
-//         session_report,
-//     })
-// }
+    let (consumption_based_kw, consumption_based_kva) =
+        max_kw_kva_over_groups_based_on_consumption(&groups);
+    let (breaker_specs_based_kw, breaker_specs_based_kva) =
+        max_kw_kva_over_groups_based_on_breaker_specs(&groups);
+
+    let estimates = PowerEstimates {
+        consumption_based_kw,
+        consumption_based_kva,
+        breaker_specs_based_kw,
+        breaker_specs_based_kva,
+    };
+
+    Ok(PowerEstimatesReport {
+        estimates,
+        spikes,
+        warnings,
+    })
+}
 
 /// Estimate of EV charging peak (kW, kVA) in the interval that was used to compute `groups`,
 /// based on the grouping of sessions and their average power draw.
-fn max_kw_kva_over_groups_based_on_consumption<'a>(
-    groups: &'a [SessionGroup<'a>],
-) -> (PowerEstimate<'a>, PowerEstimate<'a>) {
+fn max_kw_kva_over_groups_based_on_consumption(
+    groups: &[SessionGroup],
+) -> (PowerEstimate, PowerEstimate) {
     let mut group_iter = groups.iter();
     let max_group = if let Some(group0) = group_iter.next() {
         let mut max_group = group0;
@@ -83,9 +101,9 @@ fn max_kw_kva_over_groups_based_on_consumption<'a>(
 
 /// Estimate of EV charging peak (kW, kVA) in the interval that was used to compute `groups`,
 /// based on the grouping of sessions and the Evolute breaker specs.
-fn max_kw_kva_over_groups_based_on_breaker_specs<'a>(
-    groups: &'a [SessionGroup<'a>],
-) -> (PowerEstimate<'a>, PowerEstimate<'a>) {
+fn max_kw_kva_over_groups_based_on_breaker_specs(
+    groups: &[SessionGroup],
+) -> (PowerEstimate, PowerEstimate) {
     let max_group = max_depth_group(groups);
     let max_depth = max_group.session_count();
     let kw_est = PowerEstimate {
@@ -100,7 +118,7 @@ fn max_kw_kva_over_groups_based_on_breaker_specs<'a>(
 }
 
 /// Returns the [`SessionGroup`] in `groups` the with greatest session count.
-fn max_depth_group<'a>(groups: &'a [SessionGroup<'a>]) -> SessionGroup<'a> {
+fn max_depth_group(groups: &[SessionGroup]) -> SessionGroup {
     let mut group_iter = groups.iter();
     if let Some(group0) = group_iter.next() {
         let mut max_group = group0;
