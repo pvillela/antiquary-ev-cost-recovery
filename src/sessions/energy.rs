84902c5e5472e40212af03018f7a160c409ae106
pvillela <pvillela@gmail.com>
2026-08-15 17:19:33 -0400

    Renamed sessions::energy::tou_kwh_in_time_span to tou_kwh.

--- src/sessions/energy.rs
+++ src/sessions/energy.rs
@@ -9,7 +9,7 @@ pub struct TouKkh {
     pub off_peak: f64,
 }
 
-pub fn tou_kwh_in_time_span(time_span: Interval, sessions: &[Session]) -> TouKkh {
+pub fn tou_kwh(time_range: Interval, sessions: &[Session]) -> TouKkh {
     let mut on_peak_kwh = 0.0;
     let mut mid_peak_kwh = 0.0;
     let mut off_peak_kwh = 0.0;
@@ -17,7 +17,7 @@ pub fn tou_kwh_in_time_span(time_span: Interval, sessions: &[Session]) -> TouKkh
     for s in sessions {
         let session_interval = Interval::new(s.adj_conn_start(), s.adj_duration());
         let kwh_per_sec = s.energy_use / s.adj_duration().as_secs_f64();
-        let overlap = session_interval.intersection(&time_span);
+        let overlap = session_interval.intersection(&time_range);
         let partition = tou_partition(overlap);
         for (tou, itvl) in partition {
             let tou_kwh = kwh_per_sec * itvl.duration.as_secs_f64();
